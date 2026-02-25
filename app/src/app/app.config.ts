78cc53135e54cf35ad2a78b08beab7697464a6df
melissa-gv <melissagilv@gmail.com>
2026-02-24 18:11:39 -0600

    Re-org /app files

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -8,6 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes),
   ]
 };

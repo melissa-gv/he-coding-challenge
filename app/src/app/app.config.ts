bec5538f1ee4e6338646f9d02d6c0ed4ac171bff
melissa-gv <melissagilv@gmail.com>
2026-02-24 18:23:22 -0600

    Prettier

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
     provideRouter(routes),
+    providePrimeNG({theme: { preset: Aura }})
   ]
 };
